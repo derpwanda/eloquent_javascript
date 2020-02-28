c0209c916e928972d792e9d9857adb2b95932ba2
derpWanda <derpWanda@gmail.com>
2020-02-28 14:25:08 -0600

    chapter 2 completed exercises

---
+++ ejs_ch02/fizzbuzz.js
@@ -0,0 +1,25 @@
+/*
+    Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions:
+        For numbers divisible by 3, print "Fizz"
+        For numbers divisible by 5, print "Buzz"
+        For numbers divisible by 15, print "FizzBuzz"
+*/
+
+function FizzBuzz(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 15 == 0) {
+            console.log("Fizz Buzz")
+        }
+        else if (i % 3 == 0) {
+            console.log("Fizz")
+        }
+        else if (i % 5 == 0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+FizzBuzz(100)
